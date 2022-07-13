fe9737d163702331c6b73c3ea130194d694565ef
Andrey ligiza <andrey.ligiza@mail.ru>
2022-07-13 16:42:15 +0300

    авторизация и роль middleware

--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -1,8 +1,10 @@
 const Router = require('express')
 const router = new Router()
 const typeController  = require('../controllers/typeController')
+const { check } = require('../controllers/userController')
+const checkRole  = require('../middleware/checkRoleMiddleware')
 
-router.post('/', typeController.create)
+router.post('/', checkRole('ADMIN'), typeController.create)
 router.get('/', typeController.getAll)
 
 /* router.get = ('/', (req,res) => {
